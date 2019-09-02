d3a79deff6443dba15c028b57e188ecc3c87e921
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-02 16:39:42 +0530

    Add a custom Question class
    * Add Question class
    * Replace separate lists of questions and answers with a list of Questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,16 +27,12 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List<bool> answers = [
-    false,
-    true,
-    true,
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
   int currentQuestionNumber = 0;
 
@@ -67,7 +64,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[currentQuestionNumber],
+                questions[currentQuestionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -91,7 +88,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                checkAnswer(true, answers[currentQuestionNumber]);
+                checkAnswer(
+                    true, questions[currentQuestionNumber].questionAnswer);
                 moveToNextQuestion();
               },
             ),
@@ -110,7 +108,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                checkAnswer(false, answers[currentQuestionNumber]);
+                checkAnswer(
+                    false, questions[currentQuestionNumber].questionAnswer);
                 moveToNextQuestion();
               },
             ),
